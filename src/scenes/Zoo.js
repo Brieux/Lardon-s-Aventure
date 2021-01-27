09c70bfae74facaf1beec4da405994ea6f5ddca6
Brieux "Thazerr" Caquelin <33527111+Brieux@users.noreply.github.com>
2021-01-28 00:23:21 +0100

    ajout de 3 monstres

--- src/scenes/Zoo.js
+++ src/scenes/Zoo.js
@@ -41,13 +41,89 @@ class Zoo extends Tableau{
             this.monstre.setVelocityX(- (Phaser.Math.Between(45,55)));
         }
         this.physics.add.overlap(this.player, this.monstre, this.hitSpike, null, this);
+
+        //Proto 3
+        this.monstre2=this.physics.add.sprite(300,this.sys.canvas.height-70,'proto ');
+        this.monstre2.setOrigin(-9,5);
+        this.monstre2.setDisplaySize(64,64);
+        this.monstre2.setCollideWorldBounds(false);
+        this.monstre2.setBounce(0);
+        this.monstre2.setVelocityX(-100);
+        this.physics.add.overlap(this.player, this.monstre2, this.hitSpike, null, this);
+        this.monstre2.body.allowGravity = false;
+        
+        //Proto 4
+        this.monstre3=this.physics.add.sprite(300,this.sys.canvas.height-70,'proto ');
+        this.monstre3.setOrigin(-7,5);
+        this.monstre3.setDisplaySize(64,64);    
+        this.monstre3.setVelocityY(-100);
+        this.physics.add.overlap(this.player, this.monstre3, this.hitSpike, null, this);
+        this.monstre3.body.allowGravity = false;
+
+        //Proto 5
+        this.monstre4=this.physics.add.sprite(300,this.sys.canvas.height-70,'proto ');
+        this.monstre4.setOrigin(-1,3);
+        this.monstre4.setDisplaySize(64,64);    
+        this.physics.add.overlap(this.player, this.monstre4, this.hitSpike, null, this);
+        this.monstre4.body.allowGravity = false;
+        this.monstre4.cercle = true;
+        this.monstre4.setVelocityX(75);
+        this.monstre4.setVelocityY(75); 
+        this.monstre4.velocityX = 75;
+        this.monstre4.velocityY = 0;
+        this.monstre4.boolX = true;
+        this.monstre4.boolY = false;
     }
 
     update(){
         super.update();
         this.sky.tilePositionX++;
         this.monstre1.setVelocityX((this.player.x - this.monstre1.x)%60);
-        console.log((Math.abs(this.player.x - this.monstre1.x))%60)
+        //console.log((Math.abs(this.player.x - this.monstre1.x))%60)
+        if (this.monstre2.x < -700){
+            //destroy this
+        }
+        if(this.monstre3.y < 400){
+            this.monstre3.setVelocityY(100);
+        }
+        if(this.monstre3.y > 600){
+            this.monstre3.setVelocityY(-100);
+        }
+
+        //console.log(this.monstre4.cercle)
+            if (this.monstre4.boolX){
+                this.monstre4.setVelocityX(this.monstre4.velocityX -1);
+                this.monstre4.velocityX = this.monstre4.velocityX -1;
+                if (this.monstre4.velocityX == -75){
+                    this.monstre4.boolX = !this.monstre4.boolX;
+                }
+            }
+            if (!this.monstre4.boolX){
+                this.monstre4.setVelocityX(this.monstre4.velocityX +1);
+                this.monstre4.velocityX = this.monstre4.velocityX +1;
+                if (this.monstre4.velocityX == 75){
+                    this.monstre4.boolX = !this.monstre4.boolX;
+                }
+            }
+            if (this.monstre4.boolY){
+                this.monstre4.setVelocityY(this.monstre4.velocityY -1);
+                this.monstre4.velocityY = this.monstre4.velocityY -1;
+                if (this.monstre4.velocityY == -75){
+                    this.monstre4.boolY = !this.monstre4.boolY;
+                }
+            }
+            if(!this.monstre4.boolY){
+                this.monstre4.setVelocityY(this.monstre4.velocityY +1);
+                this.monstre4.velocityY = this.monstre4.velocityY +1;
+                if (this.monstre4.velocityY == 75){
+                    this.monstre4.boolY = !this.monstre4.boolY;
+                }
+            }
+            /*
+            console.log(this.monstre4.boolX);
+            console.log(this.monstre4.boolY);
+            console.log(this.monstre4.velocityX);
+            console.log(this.monstre4.velocityY);*/
     }
 
 }
